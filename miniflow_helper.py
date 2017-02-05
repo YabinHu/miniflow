c0d23e9bba0c18081843f86e43da69e068526625
Yabin Hu <huyabin@cug.edu.cn>
2017-02-06 00:39:55 +0800

    Add sgd_update function
    'sgd_update' function updates the value of each trainable with
    stochastic gradient descent.

--- miniflow_helper.py
+++ miniflow_helper.py
@@ -73,3 +73,18 @@ def forward_and_backward(graph):
     # Backward pass
     for n in graph[::-1]:
         n.backward()
+
+def sgd_update(trainables, learning_rate=1e-2):
+    """
+    Updates the value of each trainable with SGD.
+
+    Arguments:
+
+        `trainables`: A list of `Input` Nodes representing weights/biases.
+        `learning_rate`: The learning rate.
+    """
+    # TODO: update all the `trainables` with SGD
+    # You can access and assign the value of a trainable with `value` attribute.
+    # Example:
+    for t in trainables:
+        t.value -= learning_rate * t.gradients[t]
