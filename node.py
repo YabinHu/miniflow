8514e0bc25925cc453901cbd4239a22f2dd1e607
Yabin Hu <huyabin@cug.edu.cn>
2017-02-05 21:19:49 +0800

    Implement Input class
    Input is a subclass of Node. But it does not actually calculate
    anything. The Input subclass just holds a value, such as a data feature
    or a model parameter (weight/bias).

--- node.py
+++ node.py
@@ -1,21 +1,40 @@
 class Node(object):
-  def __init__(self):
-    # Node(s) from which this Node receives values
-    self.inbound_nodes = inbound_nodes
-    # Node(s) to which this Node passes values
-    self.outbound_nodes = []
-    # For each inbound Node here, add this Node as an outbound to that Node.
-    for n in self.inbound_nodes:
-      n.outbound_nodes.append(self)
+    def __init__(self):
+        # Node(s) from which this Node receives values
+        self.inbound_nodes = inbound_nodes
+        # Node(s) to which this Node passes values
+        self.outbound_nodes = []
+        # For each inbound Node here, add this Node as an outbound to that Node.
+        for n in self.inbound_nodes:
+            n.outbound_nodes.append(self)
 
-    # A calculated value
-    self.value = None
+        # A calculated value
+        self.value = None
 
-  def forward(self):
-    """
-    Forward propagation.
+    def forward(self):
+        """
+        Forward propagation.
 
-    Compute the output value based on `inbound_nodes` and store the result in
-    self.value.
-    """
-    raise NotImplemented
+        Compute the output value based on `inbound_nodes` and store the result
+        in self.value.
+        """
+        raise NotImplemented
+
+class Input(Node):
+    def __init__(self):
+        # An Input node has no inbound nodes,
+        # so no need to pass anything to the Node instantiator.
+        Node.__init__(self)
+
+    # NOTE: Input node is the only node where the value
+    # may be passed as an argument to forward().
+    #
+    # All other node implementations should get the value
+    # of the previous node from self.inbound_nodes
+    #
+    # Example:
+    # val0 = self.inbound_nodes[0].value
+    def forward(self, value=None):
+        # Overwrite the value if one is passed in.
+        if value is not None:
+            self.value = value
