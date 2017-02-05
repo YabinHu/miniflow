444d97288c0fd80adf4077477336c98bfea140cc
Yabin Hu <huyabin@cug.edu.cn>
2017-02-05 20:56:03 +0800

    Initialize inbound and outbound Nodes of a Node

--- node.py
+++ node.py
@@ -1,3 +1,9 @@
 class Node(object):
   def __init__(self):
-    # Properties will go here!
+    # Node(s) from which this Node receives values
+    self.inbound_nodes = inbound_nodes
+    # Node(s) to which this Node passes values
+    self.outbound_nodes = []
+    # For each inbound Node here, add this Node as an outbound to that Node.
+    for n in self.inbound_nodes:
+      n.outbound_nodes.append(self)
