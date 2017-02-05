82e618334a50e72b0a83c0bfbf5ebcf54be38db1
Yabin Hu <huyabin@cug.edu.cn>
2017-02-05 23:56:04 +0800

    Add MSE class
    MSE is a subclass of Node which calculates the cost using the mean
    squared error (MSE).

--- miniflow.py
+++ miniflow.py
@@ -105,3 +105,31 @@ class Sigmoid(Node):
         Your code here!
         """
         self.value = self._sigmoid(self.inbound_nodes[0].value)
+
+class MSE(Node):
+    def __init__(self, y, a):
+        """
+        The mean squared error cost function.
+        Should be used as the last node for a network.
+        """
+        # Call the base class' constructor.
+        Node.__init__(self, [y, a])
+
+    def forward(self):
+        """
+        Calculates the mean squared error.
+        """
+        # NOTE: We reshape these to avoid possible matrix/vector broadcast
+        # errors.
+        #
+        # For example, if we subtract an array of shape (3,) from an array of shape
+        # (3,1) we get an array of shape(3,3) as the result when we want
+        # an array of shape (3,1) instead.
+        #
+        # Making both arrays (3,1) insures the result is (3,1) and does
+        # an elementwise subtraction as expected.
+        y = self.inbound_nodes[0].value.reshape(-1, 1)
+        a = self.inbound_nodes[1].value.reshape(-1, 1)
+        # TODO: your code here
+        e = y - a
+        self.value = np.mean(np.square(e))
