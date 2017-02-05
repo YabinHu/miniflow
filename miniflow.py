aa14aa59ddbeea2103394ff7c0f346463f9f9e7c
Yabin Hu <huyabin@cug.edu.cn>
2017-02-05 23:40:08 +0800

    Add Sigmoid class
    Sigmoid is a subclass which performs sigmoid activation function.

--- miniflow.py
+++ miniflow.py
@@ -74,3 +74,36 @@ class Linear(Node):
         """
         self.value = np.dot(self.inbound_nodes[0].value, self.inbound_nodes[1].value)
         self.value += self.inbound_nodes[2].value
+
+class Sigmoid(Node):
+    """
+    You need to fix the `_sigmoid` and `forward` methods.
+    """
+    def __init__(self, node):
+        Node.__init__(self, [node])
+
+    def _sigmoid(self, x):
+        """
+        This method is separate from `forward` because it
+        will be used later with `backward` as well.
+
+        `x`: A numpy array-like object.
+
+        Return the result of the sigmoid function.
+
+        Your code here!
+        """
+        ev = np.exp(x)
+        return ev / (1 + ev)
+
+
+    def forward(self):
+        """
+        Set the value of this node to the result of the
+        sigmoid function, `_sigmoid`.
+
+        Your code here!
+        """
+        # This is a dummy value to prevent numpy errors
+        # if you test without changing this method.
+        self.value = self._sigmoid(self.inbound_nodes[0].value)
